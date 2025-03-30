254e7be939c869d43b76c3d33d5756a0975cf294
Tracer786 <msingh6_be20@thapar.edu>
2025-03-30 11:05:57 +0530

    Set up the PORT
    we set the  port in '.env' file. And also call the same in server file, and also make a callback to print on the console to check on which PORT our server is running

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const app = require('./app');
+const port = process.env.PORT || 3000;
 
 //create server
 const server = http.createServer(app);
 
-server.listen(3000); //on which port to run the server
+// server.listen(3000); //on which port to run the server
 
-//if we don't want to use the fixed port and want env. variable to tell which port we are using 
+//if we don't want to use the fixed port and want env. variable to tell which port we are using
 //install two more packages -> 'npm i dotenv cors'
+
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//listen on port, and make a callback to print on console whenever the server starts
