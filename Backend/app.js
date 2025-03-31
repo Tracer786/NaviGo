f635b116388ca84fdc0b3a9533044541159a9acd
Tracer786 <msingh6_be20@thapar.edu>
2025-03-31 11:13:25 +0530

    Make a call in app.js to Db
    Now we want to use DB after the connection. for that we would require Db in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();        //configure dotenv at top
 const express = require('express');
 const cors = require('cors');
+const connectToDb = require('./db/db');
 const app = express();
+
+connectToDb();
+
 app.use(cors());
 
 
