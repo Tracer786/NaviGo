1da033e9358d937b8e9a8de3b491b44e308ee55a
Tracer786 <msingh6_be20@thapar.edu>
2025-03-31 11:07:45 +0530

    Connect function
    we write the function in db.js for connection to DB

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => {
+        console.log('Connected to MongoDB');
+    });
+}
