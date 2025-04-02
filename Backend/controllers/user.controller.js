285e7531e7fac1f3126415ac0c2ee44a4ffc1771
Tracer786 <msingh6_be20@thapar.edu>
2025-04-02 22:27:55 +0530

    Error passing
    Will handle the errors that we get for the data input

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -4,6 +4,10 @@ const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
 module.exports.registerUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     
 }
 
