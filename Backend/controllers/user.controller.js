d1ef34bac6b69b02c4030279ba722422e0737a72
Tracer786 <msingh6_be20@thapar.edu>
2025-04-02 22:32:52 +0530

    Input User
    Take the user credentials and hash the password before storing it to DB.

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -8,6 +8,15 @@ module.exports.registerUser = async (req, res, next) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+    //else -> if everything is input correct
+    const { firstname, lastname, email, password } = req.body;
+
+    //and we don't store the password directly in DB
+    //will hash the password before storing it
+    const hashedPassword = await userModel.hashPassword(password);
+
+
+    //register user
     
 }
 
