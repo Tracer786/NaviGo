56ab22b7793842798b2506ffffe9ede0ace59358
Tracer786 <msingh6_be20@thapar.edu>
2025-04-05 23:06:19 +0530

    Create MiddleWare
    Created the user MiddleWare

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,3 +1,24 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+//create middleware
+moudule.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1]; //get token from cookies or headers
+    //if token is not present in cookies or headers
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    //decode token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); //verify token using secret key
+        const user = await userModel.findById(decoded.id); //find user by id in token
+        //if is added by me -> EXTRA
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        req.user = user; //set user in request object
+        return next(); //call next middleware
+    }
+}
