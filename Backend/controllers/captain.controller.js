be304052323b0236eb5172f3039b461ca57396c6
Tracer786 <msingh6_be20@thapar.edu>
2025-04-06 17:36:45 +0530

    Register Captain
    Register the captain taking all the details of captain.

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,2 +1,29 @@
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
+const { validationResult } = require('express-validator');
+
+module.exports.registerCaptain = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { fullname, email, password, vehicle } = req.body;
+        const hashedPassword = await captainModel.hashPassword(password);
+        const captain = await captainService.createCaptain({
+            firstname: fullname.firstname,
+            lastname: fullname.lastname,
+            email,
+            password: hashedPassword,
+            color: vehicle.color,
+            plate: vehicle.plate,
+            capacity: vehicle.capacity,
+            vehicleType: vehicle.vehicleType
+        });
+        //generate token for captain
+        const token = captain.generateAuthToken();
+        res.status(201).json(token,captain);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
