03e872cf68f22b4df230d3b7f6e175a6671171d4
Tracer786 <msingh6_be20@thapar.edu>
2025-04-07 21:42:16 +0530

    Login captain controller
    Create the login controller for captain

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -30,3 +30,31 @@ module.exports.registerCaptain = async (req, res) => {
     const token = captain.generateAuthToken();
     res.status(201).json({ token, captain });
 };
+
+module.exports.loginCaptain = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body; //get email and password from request body
+    //find captain by email
+    const captain = await captainModel.findOne({ email }).select("+password"); //select password field as well
+
+    //if captain not found
+    if (!captain) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    //compare password
+    const isMatch = await captain.comparePassword(password); //compare password with hashed password
+
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    //if email and password are correct
+    const token = captain.generateAuthToken(); //generate token for captain
+    res.cookie('token', token,); //set token in cookie
+
+    res.status(200).json({ token, captain });
+}
