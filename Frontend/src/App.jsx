bd45eb7781d82ccc413572fce38b679abefa3562
Tracer786 <msingh6_be20@thapar.edu>
2025-04-09 22:26:45 +0530

    Set Routes
    Will set the routes to different pages in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainSignup from "./pages/CaptainSignup";
+import CaptainLogin from "./pages/CaptainLogin";
 
 const App = () => {
     return (
         <div>
             {/* creating routes */}
             <Routes>
-                
+                <Route path="/" element={<Home />} />
+                <Route path="/userlogin" element={<UserLogin />} />
+                <Route path="/usersignup" element={<UserSignup />} />
+                <Route path="/captainlogin" element={<CaptainLogin />} />
+                <Route path="/captainsignup" element={<CaptainSignup />} />
+                {/* Add more routes as needed */}
             </Routes>
         </div>
     );
