02d601e256d3d1ae14580973463577cd91db0cf6
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-04-11 22:38:30 +0530

    2 way binding
    Updated same for password

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -4,18 +4,22 @@ import { Link } from "react-router-dom";
 const UserLogin = () => {
     // perform 2 way binding
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className="p-7 h-screen flex flex-col justify-between">
             <div>
                 <img className="w-16 mb-10" src="/images/NaviGo_Logo.png" alt="NaviGo_Logo.png" />
                 <form >
                     <h3 className="text-lg font-medium mb-2">What's your email</h3>
-                    <input required value={email} onChange={(e)=> {
-                        console.log(e.target.value)
-                    }
-                    } className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" type="email" placeholder="email@example.com" />
+                    <input required className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" type="email" placeholder="email@example.com" value={email} onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}/>
                     <h3 className="text-lg font-medium mb-2">Enter Password</h3>
-                    <input required className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" type="password" placeholder="password" />
+                    <input required className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" type="password" placeholder="password" value={password} onChange={(e)=> 
+                        {
+                            setPassword(e.target.value);
+                        }
+                    }/>
                     <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base">Login</button>
                 </form>
                 <p className="text-center">New Here?
