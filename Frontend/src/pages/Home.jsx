7c86a567c266210ccc6ccd971a349eb10624e4c0
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-04-11 22:18:51 +0530

    Set User Login Page
    Will set the user login page, set the design of the page and the routes.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <div className="bg-cover bg-bottom bg-[url(/images/Home_Image.jpg)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400">
-                <img className="w-16 ml-8" src="/images/NaviGo_Logo.png" alt="" />
+                <img className="w-16 ml-8" src="/images/NaviGo_Logo.png" alt="NaviGo_Logo.png" />
                 <div className="bg-white pb-7 py-4 px-4">
                     <h2 className="text-2xl font-bold">Get Started with NaviGo</h2>
                     <Link to='/UserLogin'  className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5">Continue</Link> 
