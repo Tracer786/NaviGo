4f464f18ecec5c23ae4b005cbfddb11dc214eb70
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-04-11 22:47:45 +0530

    submitHandler
    Will run the submit handler to check the response.

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -5,14 +5,17 @@ const UserLogin = () => {
     // perform 2 way binding
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const submitHandler = () => {
+    const submitHandler = (e) => {
+        e.preventDefault();
         console.log("Hello!");
     }
     return (
         <div className="p-7 h-screen flex flex-col justify-between">
             <div>
                 <img className="w-16 mb-10" src="/images/NaviGo_Logo.png" alt="NaviGo_Logo.png" />
-                <form onSubmit={submitHandler}>
+                <form onSubmit={(e)=>{
+                    submitHandler(e);
+                }}>
                     <h3 className="text-lg font-medium mb-2">What's your email</h3>
                     <input required className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" type="email" placeholder="email@example.com" value={email} onChange={(e) => {
                         setEmail(e.target.value);
