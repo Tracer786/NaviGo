f2d154df4e23c82d28114e6d299ddb31f9f28ba5
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-04-12 21:45:30 +0530

    Update the Home Page
    Will remove the background color as image is already shown.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            <div className="bg-cover bg-bottom bg-[url(/images/Home_Image.jpg)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400">
+            <div className="bg-cover bg-bottom bg-[url(/images/Home_Image.jpg)] h-screen pt-8 flex justify-between flex-col w-full">
                 <img className="w-16 ml-8" src="/images/NaviGo_Logo.png" alt="NaviGo_Logo.png" />
                 <div className="bg-white pb-7 py-4 px-4">
                     <h2 className="text-2xl font-bold">Get Started with NaviGo</h2>
