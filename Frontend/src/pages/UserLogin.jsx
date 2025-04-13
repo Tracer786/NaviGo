a2e4060216cd6034233fbb033548dd6c3bd07998
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-04-13 11:41:48 +0530

    Login Route Integration
    Now will integrate the login route with Backend, the same as Signup route.

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,17 +1,36 @@
-import React, { use, useState } from "react";
+import React, { use, useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const UserLogin = () => {
     // perform 2 way binding
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [userData, setUserData] = useState({});
-    const submitHandler = (e) => {
+
+    const [user, setUser] = useContext(UserDataContext);
+    const navigate = useNavigate();
+    
+    const submitHandler = async (e) => {
         e.preventDefault();
-        setUserData({
+
+        const userData = {
             email: email,
             password: password,
-        })
+        };
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, userData);
+        if (response.status === 200) {
+            const data = response.data;
+            setUser(data.user);
+            navigate("/home");
+        }
+
+        // setUserData({
+        //     email: email,
+        //     password: password,
+        // })
         console.log(userData);
         setEmail("");
         setPassword("");
