bdf49a0237ad8ee637b3b04f10c1c7f79cb429ad
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-05-13 10:12:25 +0530

    Update maps.router file
    Updated the file maps.router to make neat and clean code.

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -8,18 +8,21 @@ const {query} = require('express-validator');
 router.get('/get-coordinates',
     // query('address').notEmpty().withMessage('Address is required'), // Validate address parameter
     // query('address').isString().withMessage('Address must be a string'), // Validate address type
-    query('address').isLength({ min: 3, max: 100 }).withMessage('Address must be between 3 and 100 characters'), // Validate address length
+    // query('address').isLength({ min: 3, max: 100 }).withMessage('Address must be between 3 and 100 characters'), // Validate address length
+    query('address').isString().isLength({min: 3,max: 100}).notEmpty().withMessage('Address must be a string and between 3 and 100 characters'), // Validate address type and length
     authMiddleware.authUser,mapController.getCoordinates); // Get coordinates for a given address
 
 router.get('/get-distance-time',
-    query('origin').notEmpty().withMessage('Origin is required'), // Validate origin parameter
-    query('destination').notEmpty().withMessage('Destination is required'), // Validate destination parameter
-    query('origin').isString().withMessage('Origin must be a string'), // Validate origin type
-    query('destination').isString().withMessage('Destination must be a string'), // Validate destination type
-    query('origin').isLength({ min: 3, max: 100 }).withMessage('Origin must be between 3 and 100 characters'), // Validate origin length
-    query('destination').isLength({ min: 3, max: 100 }).withMessage('Destination must be between 3 and 100 characters'), // Validate destination length
+    // query('origin').notEmpty().withMessage('Origin is required'), // Validate origin parameter
+    // query('destination').notEmpty().withMessage('Destination is required'), // Validate destination parameter
+    // query('origin').isString().withMessage('Origin must be a string'), // Validate origin type
+    // query('destination').isString().withMessage('Destination must be a string'), // Validate destination type
+    // query('origin').isLength({ min: 3, max: 100 }).withMessage('Origin must be between 3 and 100 characters'), // Validate origin length
+    // query('destination').isLength({ min: 3, max: 100 }).withMessage('Destination must be between 3 and 100 characters'), // Validate destination length
+    query('origin').isString().isLength({min: 3,max: 100}).notEmpty().withMessage('Origin must be a string and between 3 and 100 characters'), // Validate origin type and length
+    query('destination').isString().isLength({min: 3,max: 100}).notEmpty().withMessage('Destination must be a string and between 3 and 100 characters'), // Validate destination type and length
     authMiddleware.authUser, mapController.getDistanceAndTime // Get distance and time between two locations
 )
-  
+
     
 module.exports = router;
