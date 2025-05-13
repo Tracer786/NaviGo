05512e66aba2abacbf149ce102c400c811d4fead
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-05-13 17:32:19 +0530

    Update the routes
    Will update the app.js with new routes.

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const mapRoutes = require('./routes/maps.routes'); // Import map routes
+const rideRoutes = require('./routes/ride.routes'); // Import ride routes
 
 connectToDb();
 app.use(cookiesParser()); //parse cookies
@@ -28,5 +29,6 @@ app.get('/', (req, res) => {
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes); //use captain routes
 app.use('/maps', mapRoutes); // Use map routes
+app.use('/rides', rideRoutes); // Use ride routes
 
 module.exports = app;
