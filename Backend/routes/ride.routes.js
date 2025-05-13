f188ed461e571426346ee742efdc977edf16b2a8
Khushpreet Singh <msingh6_be20@thapar.edu>
2025-05-13 11:09:52 +0530

    Create Ride Router
    Will need to make the ride creation router for user. Define different checks for the same.

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {body} = require('express-validator');
+
+//creating a ride for user
+router.post('/create',
+    body('userId').isString().isLength({min: 24,max: 24}).notEmpty().withMessage('Invalid user ID'), // Validate userID type and length
+    body('pickup').isString().isLength({min: 3,max: 100}).notEmpty().withMessage('Invalid pickup address'), // Validate pickup type and length
+    body('destination').isString().isLength({min: 3,max: 100}).notEmpty().withMessage('Invalid destination address'), // Validate destination type and length
+)
 
 module.exports = router;
